732f5145a7d002ae8df21758cc970ba0305409b2
Dennis Fischer <fischer_dennis@live.de>
2015-01-10 23:26:41 +0100

    Initial commit

---
+++ StyleCop.AnalyzersAnalyzer/DiagnosticInformation.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Text.RegularExpressions;
+
+namespace StyleCop.AnalyzersAnalyzer
+{
+    public class DiagnosticInformation
+    {
+        public string Id { get; }
+        public string Name { get; }
+        public string Type { get; }
+        public bool Finished { get; }
+        public bool Enabled { get; }
+
+        private DiagnosticInformation(string id, string name, string type, bool finished, bool enabled)
+        {
+            Id = id;
+            Name = name;
+            Type = type;
+            Finished = finished;
+            Enabled = enabled;
+        }
+
+        public static DiagnosticInformation Create(SyntaxTree tree, Compilation compilation)
+        {
+            Regex diagnosticPathRegex = new Regex(@"(?<type>[A-Za-z]+)Rules\\SA(?<id>[0-9]{4})(?<name>[A-Za-z]+)\.cs$");
+            var match = diagnosticPathRegex.Match(tree.FilePath);
+            if (!match.Success)
+            {
+                return null;
+            }
+            string id = match.Groups["id"].Value;
+            string name = match.Groups["name"].Value;
+            string type = match.Groups["type"].Value;
+            bool finished = true;
+            bool enabled = true;
+            var diagnosticAnalyzer = compilation.GetTypeByMetadataName(typeof(DiagnosticAnalyzer).FullName);
+            var semanticModel = compilation.GetSemanticModel(tree);
+            var syntaxRoot = tree.GetRoot();
+
+
+
+            // Check if diagnostic is implemented
+            foreach (var trivia in syntaxRoot.DescendantTrivia())
+            {
+                if (trivia.IsKind(SyntaxKind.SingleLineCommentTrivia))
+                {
+                    if (trivia.ToFullString().Contains("TODO: Implement analysis"))
+                    {
+                        finished = false;
+                    }
+                }
+            }
+
+            // Check if it is disabled
+            foreach (var node in syntaxRoot.DescendantNodes())
+            {
+                if (node.IsKind(SyntaxKind.ClassDeclaration))
+                {
+                    var typeInfo = semanticModel.GetDeclaredSymbol(node as ClassDeclarationSyntax);
+
+                    if(typeInfo != null)
+                    {
+                        if (typeInfo.BaseType != diagnosticAnalyzer)
+                        {
+                            return null;
+                        }
+                    }
+                }
+                if (node.IsKind(SyntaxKind.VariableDeclarator))
+                {
+                    VariableDeclaratorSyntax syntax = node as VariableDeclaratorSyntax;
+
+                    if(syntax != null)
+                    {
+                        if(syntax.Identifier.Text == "Descriptor")
+                        {
+                            EqualsValueClauseSyntax equalValueClauseSyntax = syntax.Initializer as EqualsValueClauseSyntax;
+                            if(equalValueClauseSyntax != null)
+                            {
+                                var objectCreationSyntax = equalValueClauseSyntax.Value as ObjectCreationExpressionSyntax;
+
+                                var diagnosticDescriptor = compilation.GetTypeByMetadataName(typeof(DiagnosticDescriptor).FullName);
+
+                                var symbolInfo = semanticModel.GetSymbolInfo(objectCreationSyntax);
+
+                                var symbol = symbolInfo.Symbol;
+
+                                if(symbol?.ContainingType == diagnosticDescriptor)
+                                {
+
+                                    if (objectCreationSyntax.ArgumentList != null)
+                                    {
+                                        var arguments = objectCreationSyntax.ArgumentList.Arguments;
+                                        var possibleArgument = arguments[5] as ArgumentSyntax;
+                                        if(possibleArgument != null)
+                                        {
+                                            if(possibleArgument.NameColon == null)
+                                            {
+                                                var memberAccessExpression = possibleArgument.Expression as MemberAccessExpressionSyntax;
+                                                if(memberAccessExpression != null)
+                                                {
+                                                    var identifierName = memberAccessExpression.Name.Identifier.ToString();
+                                                    enabled = identifierName != "DisabledNoTests";
+                                                    break;
+                                                }
+                                            }
+                                            else
+                                            {
+                                                System.Console.WriteLine("Not supported");
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            DiagnosticInformation info = new DiagnosticInformation(id, name, type, finished, enabled);
+
+            return info;
+        }
+    }
+    public class DiagnosticGroup
+    {
+        public string Type { get; set; }
+        public List<DiagnosticInformation> Diagnostics { get; set; }
+    }
+}
---
+++ StyleCop.AnalyzersAnalyzer/Program.cs
@@ -0,0 +1,90 @@
+﻿/// <summary>
+/// A tool to analyze the current status of the project
+/// </summary>
+namespace StyleCop.AnalyzersAnalyzer
+{
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.MSBuild;
+    using RazorEngine;
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args.Length > 0 ? args[0] : @"C:\StyleCopAnalyzers";
+
+            string solutionFile = Path.Combine(path, "StyleCopAnalyzers.sln");
+
+
+
+            if (File.Exists(solutionFile))
+            {
+                var workSpace = MSBuildWorkspace.Create();
+
+                Solution solution = workSpace.OpenSolutionAsync(solutionFile).Result;
+
+                Project project = solution.Projects.Single(x => x.Name == "StyleCop.Analyzers");
+
+                var compilation = project.GetCompilationAsync().Result;
+
+                var diagnostics = from diagnostic in compilation.SyntaxTrees.Select(x => DiagnosticInformation.Create(x, compilation))
+                                  where diagnostic != null
+                                  orderby diagnostic.Id
+                                  group diagnostic by diagnostic.Type into g
+                                  select new DiagnosticGroup { Type = g.Key, Diagnostics = g.ToList() };
+                string result = Razor.Parse(File.ReadAllText(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "HtmlGen.cshtml")), diagnostics);
+
+                Console.WriteLine(result);
+//                Console.WriteLine("<!-- Auto generated, do not change manually -->");
+//                Console.WriteLine(@"
+//<html>
+//<head>
+//<link href=""//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"" rel=""stylesheet""/>
+//<title>Status page</title>
+//</head>
+//<body style=""padding: 5em;"">");
+//                Console.WriteLine("<h1>Current status (Last update \{DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)} UTC)</h1>");
+//                foreach (var item in diagnostics)
+//                {
+//                    Console.WriteLine("<h2>" + item.Type + "</h2>");
+//                    Console.WriteLine();
+//                    Console.WriteLine("<table class=\"table table-hover table-condensed\"><thead><tr><td>ID</td><td>Name</td><td>Implemented (\{item.Diagnostics.Count(x => x.Finished)}/\{item.Diagnostics.Count})</td><td>Tests (\{item.Diagnostics.Count(x => x.Enabled)}/\{item.Diagnostics.Count})</td></thead>");
+
+//                    foreach (var diagnostic in item.Diagnostics)
+//                    {
+//                        string @class = diagnostic.Enabled ? "success" : diagnostic.Finished ? "warning" : "danger";
+//                        Console.WriteLine("<tr class=\"\{@class}\">");
+//                        Console.WriteLine("<td><a href=\"http://www.stylecop.com/docs/SA\{diagnostic.Id}.html\">SA\{diagnostic.Id}</a></td>");
+//                        Console.WriteLine("<td>" + diagnostic.Name + "</td>");
+//                        Console.WriteLine("<td>" + GetHtmlCheckbox(diagnostic.Finished) + "</td>");
+//                        Console.WriteLine("<td>" + GetHtmlCheckbox(diagnostic.Enabled) + "</td>");
+//                        Console.WriteLine("</tr>");
+//                    }
+
+//                    Console.WriteLine("</table>");
+//                    Console.WriteLine();
+//                }
+//                Console.WriteLine("</body>");
+            }
+        }
+
+
+
+        private static string GetHtmlCheckbox(bool finished)
+        {
+            return "<input type=\"checkbox\" " + (finished ? "checked=\"checked\"" : "") + "  disabled=\"disabled\" />";
+        }
+    }
+    public class DiagnosticGroup
+    {
+        public string Type { get; set; }
+        public List<DiagnosticInformation> Diagnostics { get; set; }
+    }
+}
